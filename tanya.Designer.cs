74c759628335bfc8d564b9ce3e4dc3ddd050ab7a
Ikhsanarya <63132100+Ikhsanarya@users.noreply.github.com>
2020-05-15 16:12:13 +0700

    Add files via upload

---
+++ tanya.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Kyou_Hobby_shop
+{
+    partial class konfirmasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTanya = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTanya
+            // 
+            this.labelTanya.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelTanya.Location = new System.Drawing.Point(103, 79);
+            this.labelTanya.Name = "labelTanya";
+            this.labelTanya.Size = new System.Drawing.Size(108, 43);
+            this.labelTanya.TabIndex = 0;
+            this.labelTanya.Text = "Yakin akan keluar ?";
+            this.labelTanya.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelTanya.Click += new System.EventHandler(this.labelTanya_Click);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(43, 125);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(90, 25);
+            this.buttonYes.TabIndex = 1;
+            this.buttonYes.Text = "&Yes";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(186, 125);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(90, 25);
+            this.buttonNo.TabIndex = 2;
+            this.buttonNo.Text = "&No";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BAJU_MURAH_MERIAH.Properties.Resources.kyou;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(106, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 78);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // konfirmasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(317, 173);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.labelTanya);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "konfirmasi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Konfirmasi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTanya;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Button buttonNo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
